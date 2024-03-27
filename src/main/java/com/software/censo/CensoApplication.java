2e25d454b26c078fa6bdb4efde56e5e5f6f9897e
Alejandra Gabriela Rosel Rios <aleroselrios@gmail.com>
2024-03-26 22:12:42 -0400

    First commit

---
+++ src/main/java/com/software/censo/CensoApplication.java
@@ -0,0 +1,13 @@
+package com.software.censo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CensoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CensoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/software/censo/CensoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.censo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CensoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
