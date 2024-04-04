4935e2889082b3c2d3b9ae362b2bf8d2bcf3a2da
jaimejem23 <jaime.jem23@gmail.com>
2024-04-03 20:41:53 -0400

    Update CensoUser.java
    ID UUID, roles, creado_por

--- src/main/java/com/software/censo/model/CensoUser.java
+++ src/main/java/com/software/censo/model/CensoUser.java
@@ -3,14 +3,28 @@ package com.software.censo.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 @Entity
 public class CensoUser {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private Long id;
-    private String name;
-    private String lastName;
-    private String email;
-    private String password;
+    public UUID id;
+    public String name;
+    public String lastName;
+    public String email;
+    public String password;
+    @ManyToOne(optional = true)
+    @JoinColumn(name = "creado_por")
+    public CensoUser creadoPor;
+    @Enumerated(EnumType.STRING)
+    public TipoRol roles;
+
+
+    public enum TipoRol {
+        ADMINISTRADOR,
+        CENSADOR,
+        USUARIO
+    }
 }
